85d6a5b501c4b14f01cb0bff821a41896b0d2b5b
GuillemSuneDAW2 <guillem_sunemontana@iescarlesvallbona.cat>
2019-04-07 22:59:00 +0200

    Broken Bridge

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public enum GameState { Idle, Playing};
+    public GameState gameState = GameState.Idle;
+
+    public GameObject player;
+
+    public GameObject bridge;
+    public GameObject brokenBridge;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameState == GameState.Idle)
+        {
+
+        }
+    }
+
+    public static void RestartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void DestroyBridge()
+    {
+        bridge.SetActive(false);
+        brokenBridge.SetActive(true);
+    }
+}
